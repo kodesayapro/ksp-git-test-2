9bfbebb3d881a498910e4649ef27232bd4dc62e2
kodesayapro <arifnurrahman6789@gmail.com>
2024-09-22 13:00:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis qui beatae eos unde officiis sit iste delectus et accusamus quasi quia earum, illum aliquid hic illo incidunt modi aspernatur eum!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
